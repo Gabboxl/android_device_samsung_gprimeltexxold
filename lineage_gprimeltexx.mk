93d158a982b2bc92b5f09dbd98cfa5fdc11a7a50
DarkDroidDev <darkdroid.dev@gmail.com>
2020-01-12 20:48:50 +0100

    gprimeltexx: Remove common LineageOS's makefile in common tree

--- lineage_gprimeltexx.mk
+++ lineage_gprimeltexx.mk
@@ -13,10 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/lineage.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from common
 $(call inherit-product, device/samsung/gprimeltexx/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Apex
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
